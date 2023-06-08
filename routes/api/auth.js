ca09351b989b19a1809e9f3d96d71221e39e32b8
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-06-08 17:11:45 +0530

    user athontication login Router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth =require("../../middleware/auth")
 const User = require("../../models/User")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const config = require("config")
+const { check , validationResult } = require("express-validator")
 // @router  get api/auth
 // @desc    test route
 // @access  public 
@@ -12,6 +16,54 @@ router.get('/',auth,async (req,res)=> {
     }catch(err){
         console.error(err.message);
         res.status(500).send("Server Error")
+    }
+});
+
+// @router  post api/auth
+// @desc    authenticate user & get token 
+// @access  public 
+router.post('/',[
+    check('email','Please include  a valid email').isEmail(),
+    check('password','please is required ').exists()
+], async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { email,password} = req.body;
+    try{
+        // see if user exit or not
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({ errors: [{ msg : " invalid Email "}]});
+        }
+        
+        const isMatch = await bcrypt.compare(password , user.password);
+        
+        if(!isMatch){
+            return res.status(400).json({ errors: [{ msg : " invalid Password "}]});
+        }
+        // return jsonwebtoken
+        
+        const payload = {
+            user:
+            { id: user.id}
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn:36000},
+            (err,Token)=>{
+                if(err) throw err;
+                res.json({Token});
+            }
+        );
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
     }
 });
 

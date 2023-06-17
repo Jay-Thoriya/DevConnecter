1298c07272ad1f1885cba10fdfbab25c408f1c28
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-06-17 14:17:09 +0530

    connect register data to the backend server

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 
 const Register = () => {
+  const [fromData, setFromData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = fromData;
+
+  const onChange = e => setFromData({ ...fromData , [e.target.name]:e.target.value})
+  const onSubmit = async e =>{
+    e.preventDefault();
+    if(password !== password2){
+      console.log("password Do not match")
+    }else{
+      const newUser = {
+        name,
+        email,
+        password
+      }
+
+      try {
+        const config = {
+          Headers:{
+            'Content-Type':'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post('/api/user', body,config);
+
+        console.log(res.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+
+    }
+  }
   return (
     <section className='container'>
-    <div>
-    Register
-    </div>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
+          <small className="form-text"
+          >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} 
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2} 
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
     </section>
   )
 }

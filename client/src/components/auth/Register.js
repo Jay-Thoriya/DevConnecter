13b8b08580ef112313daf60e3e5dea160a95c821
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-06-29 21:56:33 +0530

    register user data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,22 +1,51 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom';
-const Register = () => {
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../action/alert';
+import { register } from '../../action/auth';
+import PropTypes from "prop-types"
+import {connect} from "react-redux" 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Register = ({ setAlert , register}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
     password2: ''
-  }); 
-
+  });
   const { name, email, password, password2 } = formData;
 
-  const onChange = e => setFormData({ ...formData , [e.target.name]:e.target.value})
-  const onSubmit =  e =>{
-    e.preventDefault(); 
-    if(password !== password2){
-      console.log("password Do not match")
-    }else{
-      console.log("Success")
+
+  const Wrong = () => toast.error("Wrong Password!, Try again", {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+
+    const Success = () => toast.success('Complete registration', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert("Password do not match","danger")
+      Wrong()
+    } else {
+      register({ name, email , password})
+      Success()
     }
   }
   return (
@@ -27,8 +56,8 @@ const Register = () => {
         <div className="form-group">
           <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
         </div>
-        <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
+        <div className="form-group">                                                                                      
+          <input type="email" placeholder="Email Address" name="email" value={email} required onChange={e => onChange(e)} />
           <small className="form-text"
           >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -39,7 +68,7 @@ const Register = () => {
             type="password"
             placeholder="Password"
             name="password"
-            value={password} 
+            value={password}
             onChange={e => onChange(e)}
             minLength="6"
           />
@@ -49,7 +78,7 @@ const Register = () => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            value={password2} 
+            value={password2}
             onChange={e => onChange(e)}
             minLength="6"
           />
@@ -59,9 +88,15 @@ const Register = () => {
       <p className="my-1">
         Already have an account? <Link to="/login">Sign In</Link>
       </p>
+      <ToastContainer/>
     </section>
-  )
-}
+  );
+};
 
-export default Register;
+Register.propTypes ={
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+} 
 
+
+export default connect(null ,{setAlert , register})(Register);

635e9973f4aa37f40c190d3a14adf5dac8e76d83
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-06-29 21:56:17 +0530

    making alert request when password are worrng

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
 import './App.css';
 import Home from "./components/layout/Home";
+import React ,  { useEffect } from "react";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Navbar from "./components/layout/Navbar";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./action/auth";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+
+  useEffect(()=>{
+    store.dispatch(loadUser())
+  }, []);
+
   return (
     <>
     <Provider store={store}>
